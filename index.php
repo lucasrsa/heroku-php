2a6437e959ec5c1e7d7e0885e8e75aadf1fc6642
Guilherme Solski Alves <guilhermesolskialves@gmail.com>
2021-06-21 18:33:29 -0300

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $actual_date  = date('d/m/y');
+?>
+
+<html>
+    <h1> Avaliação SOCPS II </h1>
+    <h2> Nome: Guilherme Solski Alves </h2>
+    <h2> <?= $actual_date ?> </h2>
+
+</html>
+
